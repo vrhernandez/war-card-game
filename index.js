85fe602b7795bdf6325d5c4da407abc489be6ae3
vrhernandez <vicenterh99@gmail.com>
2024-12-10 16:56:19 -0800

    added draw button

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+const cardTable = document.getElementById("card-table")
+let deckId
+
+getNewDeck()
+
 function getNewDeck() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+            deckId = data.deck_id
+        })
+}
+
+function drawCards() {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            cardTable.innerHTML = ""
+            for(let card of data.cards) {
+                console.log(`${card.value} of ${card.suit}`)
+                cardTable.innerHTML += `<img src="${card.image}">`
+            }
+        })
 }
 
 document.getElementById("new-deck").addEventListener("click", getNewDeck)
+document.getElementById("draw-cards").addEventListener("click", drawCards)
